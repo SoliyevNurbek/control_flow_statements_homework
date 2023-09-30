f62a010689a6b44ee049ac19c68c4b26077b25a9
SoliyevNurbek <soliyevnurbek4243@gmail.com>
2023-09-30 13:10:10 +0500

    f8

--- if08.py
+++ if08.py
@@ -11,4 +11,12 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if a//10>0 and a//10<10 and a%2==1 :
+        return "two-digit odd number"
+    elif a//10>0 and a//10<10 and a%2==0 :
+        return "two-digit even number"
+    elif a//100>0 and a//100<10 and a%2==1 :
+        return "three-digit odd number"
+    elif a//100>0 and a//100<10 and a%2==0 :
+        return "three-digit even number"
+print(main(124))
